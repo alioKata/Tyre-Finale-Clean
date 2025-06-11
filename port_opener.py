9cdb6250c7e79156264c6a0b06cb8f27b74dc267
amin tahmasebi <amintahmasebi479@gmail.com>
2025-06-11 20:39:34 +0330

    change in yaml file

--- port_opener.py
+++ port_opener.py
@@ -1,23 +1,20 @@
-import socket, time, os, sys, signal
+import socket, os, sys, signal
 
 PORT = int(os.environ.get('PORT', 10000))
 print(f"Starting port opener on port {PORT}")
 
-# Clean shutdown on SIGTERM / SIGINT
-def shutdown(signum, frame):
-    print("Shutdown signal received, exiting.")
+def shutdown(sig, frame):
+    print("Shutting down…")
     sys.exit(0)
 signal.signal(signal.SIGTERM, shutdown)
 signal.signal(signal.SIGINT, shutdown)
 
-# Bind & serve forever
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('0.0.0.0', PORT))
-    server.listen(5)
-    print(f"Server listening on port {PORT}")
+with socket.socket() as srv:
+    srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv.bind(('0.0.0.0', PORT))
+    srv.listen(5)
+    print("Server listening…")
     while True:
-        conn, addr = server.accept()
-        print(f"Connection from {addr}")
+        conn, _ = srv.accept()
         conn.send(b"HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK")
         conn.close()
